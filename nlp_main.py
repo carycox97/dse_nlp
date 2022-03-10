c7f9d7a6d86f8e6bda4d17c6a35c037088a9caef
Cox <carycox97@gmail.com>
2022-03-10 10:32:07 -0500

    created bigrams_by_percentage function for prof skills

--- nlp_main.py
+++ nlp_main.py
@@ -3483,8 +3483,7 @@ def visualize_n_grams(n_grams, ds_cred_terms, ds_tech_skill_terms, ds_soft_skill
                              order=df_jobs_mono_sns.sort_values('percentage', ascending = False).ds_prof_skill_term[:25],
                              orient='h',
                              palette='mako_r') # crest, mako, 'mako_d, Blues_d, mako_r, ocean, gist_gray, gist_gray_r, icefire
-            
-            
+                        
             ax.set_title(textwrap.fill('**For Prof Parsing: Monograms by Percentage', width=40), # original title: Percentage Key Terms for Data Scientist Credentials
                          fontsize=24,
                          loc='center')
@@ -3504,6 +3503,93 @@ def visualize_n_grams(n_grams, ds_cred_terms, ds_tech_skill_terms, ds_soft_skill
                         wrap=True)  
             
             return df_jobs_mono
+
+
+        def bigrams_by_percentage(df_jobs_raw, bigram_match_to_prof_list): 
+            '''
+            Visualize the professional skills bigrams as a function of percentage of listings in which the monogram appears.
+
+            Parameters
+            ----------
+            df_jobs_raw : dataframe
+                A dataframe wherein each record is a unique listing, and each term in each listing is tokenized. df_jobs_raw is
+                created in the visualize_n_grams function so that it can be used in subfunctions.
+            bigram_match_to_prof_list : list
+                A list of bigrams in which each bigram has at least one term matching a term in the ds_prof_skill_terms list.
+
+            Returns
+            -------
+            df_jobs_bigrams : dataframe
+                A dataframe wherein each record is a job listing, and each column is a boolean flag for each
+                bigram in the bigram_match_to_prof_list list.  The final row and column each contain totals for their 
+                respective job listing and professional skill bigram, respectively. The job_description field is dropped
+                before the summations.
+
+            '''               
+            # create df_jobs_bigrams from a copy of df_jobs_raw
+            df_jobs_bigrams = df_jobs_raw.copy()
+            
+            # flag job listings if they contain the technical skill term (from stack question)
+            def find_bigram_match_to_prof_list(data):
+                output = np.zeros((data.shape[0], len(bigram_match_to_prof_list)), dtype=bool)
+                for i, d in enumerate(data):
+                    possible_bigrams = [' '.join(x) for x in list(nltk.bigrams(d)) + list(nltk.bigrams(d[::-1]))]
+                    indices = np.where(np.isin(bigram_match_to_prof_list, list(set(bigram_match_to_prof_list).intersection(set(possible_bigrams)))))
+                    output[i, indices] = True
+                return list(output.T)
+    
+            output = find_bigram_match_to_prof_list(df_jobs_bigrams['job_description'].to_numpy())
+            df_jobs_bigrams = df_jobs_bigrams.assign(**dict(zip(bigram_match_to_prof_list, output)))
+            
+            # identify and silence noisy, duplicate or unhelpful bigrams 
+            bigrams_to_silence = ['machine learning', 'experience experience', 'collaborate collaborate']
+            df_jobs_bigrams = df_jobs_bigrams.drop(columns=bigrams_to_silence)
+            
+            # calculate sum of all technical skill terms for both rows and columns
+            df_jobs_bigrams = df_jobs_bigrams.drop('job_description', axis=1)
+            df_jobs_bigrams.loc[:, 'total_bigram_in_list'] = df_jobs_bigrams.sum(axis=1) # this does rows; need to plot these to filter out noisy/broken listings; can be used for the unicorn index
+            df_jobs_bigrams.loc['total_bigram', :] = df_jobs_bigrams.sum(axis=0) # this does columns; need to drop the job_description field
+            
+            # drop all rows except the total row, transform columns and rows and rename the fields
+            df_jobs_bigrams_sns = df_jobs_bigrams.drop(df_jobs_bigrams.index.to_list()[:-1], axis = 0).melt()
+            df_jobs_bigrams_sns.rename(columns={'variable': 'ds_prof_term','value': 'count'}, inplace=True)
+            
+            # calculate a percentages field
+            df_jobs_bigrams_sns['percentage'] = [round(x / len(df_jobs_raw)*100, 2) for x in df_jobs_bigrams_sns['count']]
+            df_jobs_bigrams_sns = df_jobs_bigrams_sns[df_jobs_bigrams_sns['ds_prof_term'].str.contains('total')==False]
+            
+            # create a horizontal barplot visualizing data science credentials as a percentage of job listings
+            plt.figure(figsize=(7, 10))
+            sns.set_style('dark')
+            sns.set(font_scale = 1.8)               
+       
+            ax = sns.barplot(x='percentage',
+                             y='ds_prof_term',
+                             data=df_jobs_bigrams_sns,
+                             order=df_jobs_bigrams_sns.sort_values('percentage', ascending = False).ds_prof_term[:25],
+                             orient='h',
+                             palette='mako_r') # crest, mako, 'mako_d, Blues_d, mako_r, ocean, gist_gray, gist_gray_r, icefire
+            
+            ax.set_title(textwrap.fill('**For Prof Parsing: Bigrams by Percentage', width=40), # original title: Percentage Key Bigrams for Data Scientist Credentials
+                         fontsize=24,
+                         loc='center')
+            ax.set(ylabel=None)
+            ax.set_xlabel('Percentage', fontsize=18)
+            
+            plt.figtext(0.330, 0.010,
+                        textwrap.fill(f'Data: {len(df)} Indeed job listings for "data scientist" collected between {min(df.scrape_date)} and {max(df.scrape_date)}',
+                                      width=60),
+                        bbox=dict(facecolor='none', boxstyle='square', edgecolor='none', pad=0.2),
+                        fontsize=14,
+                        color='black',
+                        fontweight='regular',
+                        style='italic',
+                        ha='left',
+                        in_layout=True,
+                        wrap=True) 
+            
+            return df_jobs_bigrams
+
 
     # create a clean dataframe where each record is a unique listing, and each term is tokenized
     df_jobs_raw = clean_listings_for_nlp(series_of_interest, additional_stopwords, term_fixes) 
